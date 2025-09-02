971de0fee31257b7781e692f15420583edd61c88
water <s23078997@gmail.com>
2025-09-02 18:05:31 +0800

    初始化spring boot project

---
+++ springboog-mall/src/main/java/com/water/springboogmall/SpringboogMallApplication.java
@@ -0,0 +1,13 @@
+package com.water.springboogmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboogMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboogMallApplication.class, args);
+	}
+
+}
---
+++ springboog-mall/src/test/java/com/water/springboogmall/SpringboogMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water.springboogmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboogMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
