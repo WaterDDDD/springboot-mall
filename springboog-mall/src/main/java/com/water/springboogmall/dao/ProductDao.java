041c1ce1839fee8a3e4656114112d195d7dccca3
water <s23078997@gmail.com>
2025-09-03 17:56:42 +0800

    商品功能- 實作查詢商品

---
+++ springboog-mall/src/main/java/com/water/springboogmall/dao/ProductDao.java
@@ -0,0 +1,8 @@
+package com.water.springboogmall.dao;
+
+import com.water.springboogmall.model.Product;
+
+public interface ProductDao {
+
+    Product getProductById(Integer productId);
+}
---
+++ springboog-mall/src/main/java/com/water/springboogmall/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.water.springboogmall.service;
+
+import com.water.springboogmall.model.Product;
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
