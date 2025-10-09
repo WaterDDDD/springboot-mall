085e218effc88970156c4079cb73bd0ded9d7f50
water <s23078997@gmail.com>
2025-10-09 19:08:21 +0800

    商品功能 - 單元測試(修改)

--- springboog-mall/src/test/java/com/water/springboogmall/controller/ProductControllerTest.java
+++ springboog-mall/src/test/java/com/water/springboogmall/controller/ProductControllerTest.java
@@ -207,7 +207,7 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.limit", notNullValue()))
                 .andExpect(jsonPath("$.offset", notNullValue()))
                 .andExpect(jsonPath("$.total", notNullValue()))
-                .andExpect(jsonPath("$.result", hasSize(5)));
+                .andExpect(jsonPath("$.results", hasSize(5)));
     }
 
     @Test
@@ -222,7 +222,7 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.limit", notNullValue()))
                 .andExpect(jsonPath("$.offset", notNullValue()))
                 .andExpect(jsonPath("$.total", notNullValue()))
-                .andExpect(jsonPath("$.result", hasSize(2)));
+                .andExpect(jsonPath("$.results", hasSize(2)));
     }
 
     @Test
@@ -238,12 +238,12 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.limit", notNullValue()))
                 .andExpect(jsonPath("$.offset", notNullValue()))
                 .andExpect(jsonPath("$.total", notNullValue()))
-                .andExpect(jsonPath("$.result", hasSize(5)))
-                .andExpect(jsonPath("$.result[0].productId", equalTo(6)))
-                .andExpect(jsonPath("$.result[1].productId", equalTo(5)))
-                .andExpect(jsonPath("$.result[2].productId", equalTo(7)))
-                .andExpect(jsonPath("$.result[3].productId", equalTo(4)))
-                .andExpect(jsonPath("$.result[4].productId", equalTo(2)));
+                .andExpect(jsonPath("$.results", hasSize(5)))
+                .andExpect(jsonPath("$.results[0].productId", equalTo(6)))
+                .andExpect(jsonPath("$.results[1].productId", equalTo(5)))
+                .andExpect(jsonPath("$.results[2].productId", equalTo(7)))
+                .andExpect(jsonPath("$.results[3].productId", equalTo(4)))
+                .andExpect(jsonPath("$.results[4].productId", equalTo(2)));
     }
 
     @Test
@@ -259,8 +259,8 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.limit", notNullValue()))
                 .andExpect(jsonPath("$.offset", notNullValue()))
                 .andExpect(jsonPath("$.total", notNullValue()))
-                .andExpect(jsonPath("$.result", hasSize(2)))
-                .andExpect(jsonPath("$.result[0].productId", equalTo(5)))
-                .andExpect(jsonPath("$.result[1].productId", equalTo(4)));
+                .andExpect(jsonPath("$.results", hasSize(2)))
+                .andExpect(jsonPath("$.results[0].productId", equalTo(5)))
+                .andExpect(jsonPath("$.results[1].productId", equalTo(4)));
     }
 }
